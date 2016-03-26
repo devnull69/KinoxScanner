d133b3981ebbb03c89e9b3cf4f145dcd38d6cc38
Thomas Theiner <thomas.theiner@gmx.de>
2016-03-26 12:41:08 +0100

    Updated Vidzi Strategie

--- app/src/main/java/org/theiner/kinoxscanner/strategien/VidziStrategie.java
+++ app/src/main/java/org/theiner/kinoxscanner/strategien/VidziStrategie.java
@@ -31,10 +31,51 @@ public class VidziStrategie extends HosterStrategie {
         if(matcher.find()) {
             String[] matches = matcher.group(1).split("\\|");
 
-            // Scheme is: 2://40.39.38.37/80/v.79
-            response = matches[2] + "://" + matches[40] + "." + matches[39] + "." + matches[38] + "." + matches[37] + "/" + matches[80] + "/v." + matches[79];
+            // Get scheme (find v. and go back 19 positions with length 23)
+            int startpos = vidziHTML.indexOf("v.") - 19;
+            String scheme = vidziHTML.substring(startpos, startpos+23);
+
+            // Get positions from scheme
+            Pattern ptnPositions = Pattern.compile("\\b([a-z0-9]{1,2})\\b");
+            Matcher posMatcher = ptnPositions.matcher(scheme);
+            String[] positions = new String[8];
+            int current = 0;
+            while(posMatcher.find()) {
+                positions[current] = matches[getIntFromHexaTridecimal(posMatcher.group(1))];
+                if("".equals(positions[current]))
+                    positions[current] = posMatcher.group(1);
+                current++;
+            }
+
+            // Scheme is: 0://1.2.3.4/5/6.7
+            response = positions[0] + "://" + positions[1] + "." + positions[2] + "." + positions[3] + "." + positions[4] + "/" + positions[5] + "/" + positions[6] + "." + positions[7];
         }
 
         return response;
+    }
+
+    private int getIntFromHexaTridecimal(String hextri) {
+        int result = 0;
+        if(hextri.length() > 1) {
+            String first = hextri.substring(0, 1);
+            hextri = hextri.substring(1);
+            result = getIntFromHexaTridecimalDigit(first) * 36;
+        }
+        result = result + getIntFromHexaTridecimalDigit(hextri);
+        return result;
+    }
+
+    private int getIntFromHexaTridecimalDigit(String digit) {
+        int result = 0;
+        Pattern pattern = Pattern.compile("[a-z]");
+        Matcher matcher = pattern.matcher(digit);
+        if(matcher.find()) {
+            // alphabetic
+            result = ((int) digit.charAt(0)) - 87;
+        } else {
+            // numeric
+            result = Integer.parseInt(digit);
+        }
+        return result;
     }
 }
