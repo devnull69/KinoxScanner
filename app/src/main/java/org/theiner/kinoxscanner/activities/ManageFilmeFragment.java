7e068efff85e936a15ccc89f18137f55635eb15f
Thomas Theiner <thomas.theiner@gmx.de>
2016-04-08 12:45:13 +0200

    ManageFilme is now also a fragment in ViewPager

--- app/src/main/java/org/theiner/kinoxscanner/activities/ManageFilmeFragment.java
+++ app/src/main/java/org/theiner/kinoxscanner/activities/ManageFilmeFragment.java
@@ -1,25 +1,73 @@
 package org.theiner.kinoxscanner.activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 import org.theiner.kinoxscanner.R;
+import org.theiner.kinoxscanner.context.KinoxScannerApplication;
+import org.theiner.kinoxscanner.data.Film;
 
 public class ManageFilmeFragment extends Fragment {
+
+    private KinoxScannerApplication myApp;
+    private BaseAdapter adapter = null;
+    private ListView lvFilme;
+    private Activity me;
+    private Button btnNewMovie;
+
+    public final static String EXTRA_MESSAGE = "org.theiner.kinoxscanner.MESSAGEFILME";
+    public static int REQUEST_EDIT_FILM = 102;
+    public static int RESULT_UPDATE_LIST = 202;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("kinoxscanner", "ManageFilmeFragment onCreate");
+
+        me = this.getActivity();
+        myApp = (KinoxScannerApplication) me.getApplicationContext();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View layout = inflater.inflate(R.layout.fragment_manage_filme, null);
         Log.d("kinoxscanner", "ManageFilmeFragment onCreateView");
+
+        adapter = new ArrayAdapter<Film>(me, android.R.layout.simple_list_item_1, myApp.getFilme());
+        lvFilme = (ListView) layout.findViewById(R.id.lvFilme);
+        lvFilme.setAdapter(adapter);
+
+        lvFilme.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listview, View view, int position, long id) {
+                Film selected = (Film) listview.getItemAtPosition(position);
+                int currentIndex = myApp.getFilme().indexOf(selected);
+                Intent intent = new Intent(me, EditFilmActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, currentIndex);
+                startActivityForResult(intent, REQUEST_EDIT_FILM);
+            }
+        });
+
+        btnNewMovie = (Button) layout.findViewById(R.id.btnNewMovie);
+
+        btnNewMovie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNewFilm(v);
+            }
+        });
+
         return layout;
     }
 
@@ -29,5 +77,24 @@ public class ManageFilmeFragment extends Fragment {
         Log.d("kinoxscanner", "ManageFilmeFragment onActivityCreated");
     }
 
+    public void onNewFilm(View view) {
+        int currentIndex = -1;
+        Intent intent = new Intent(me, EditFilmActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, currentIndex);
+        startActivityForResult(intent, REQUEST_EDIT_FILM);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_EDIT_FILM) {
+            if(resultCode == RESULT_UPDATE_LIST) {
+                Boolean updateList = data.getBooleanExtra("updateList", false);
+                if(updateList != null && updateList) {
+                    // notify the adapter about the change
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        }
+    }
 
 }
