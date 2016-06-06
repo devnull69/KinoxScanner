71b045f007908c184df7d81f0e47320d919a27f6
Thomas Theiner <thomas.theiner@gmx.de>
2016-06-06 22:29:41 +0200

    Request ignore battery optimizations added

--- app/src/main/java/org/theiner/kinoxscanner/activities/OptionsActivity.java
+++ app/src/main/java/org/theiner/kinoxscanner/activities/OptionsActivity.java
@@ -1,10 +1,17 @@
 package org.theiner.kinoxscanner.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.PowerManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -17,6 +24,8 @@ public class OptionsActivity extends AppCompatActivity {
 
     private Switch swWifiOnly;
 
+    private Button btnBatteryOpt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,7 @@ public class OptionsActivity extends AppCompatActivity {
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         swWifiOnly = (Switch) findViewById(R.id.swWifiOnly);
+        btnBatteryOpt = (Button) findViewById(R.id.btnBatteryOpt);
 
         final SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
         final Activity me = this;
@@ -47,9 +57,33 @@ public class OptionsActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // Button nur für Android API >= 23 anzeigen
+        if(Build.VERSION.SDK_INT < 23) {
+            btnBatteryOpt.setVisibility(View.GONE);
+        } else {
+            String packageName = getPackageName();
+            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            if (pm.isIgnoringBatteryOptimizations(packageName))
+                btnBatteryOpt.setVisibility(View.GONE);
+        }
     }
 
     public void onBack(View view) {
         finish();
+    }
+
+    public void onBatteryOpt(View view) {
+        // Dialog zur Auswahl der Ausnahme für Akku-Optimierung anzeigen
+        if(Build.VERSION.SDK_INT >= 23) {
+            Intent intent = new Intent();
+            String packageName = getPackageName();
+            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package:" + packageName));
+            startActivity(intent);
+
+            btnBatteryOpt.setVisibility(View.GONE);
+        }
     }
 }
